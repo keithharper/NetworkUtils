63e75da2adc0ceaca68b78e56a735770d8f6bd70
keithharper <keithharper1155@gmail.com>
2016-04-28 17:59:35 -0400

    Cleaned up a lot of code

--- NetworkUtils/src/com/network/utils/Multicast.java
+++ NetworkUtils/src/com/network/utils/Multicast.java
@@ -9,67 +9,75 @@ import java.net.MulticastSocket;
  *
  * @author Keith Harper
  */
-public final class Multicast {
-    public MulticastSocket mcastSocket;
-    public String dataPacketString;
-    public InetAddress sourceIP;
+public final class Multicast {    
+    private final byte[] DATA_BUFFER;
+    private final DatagramPacket RECEIVED_PACKET;
+    private final InetAddress MULTICAST_GROUP_ADDRESS;
+    private final int TTL = 16;
+    
+    public MulticastSocket multicastSocket;
+    public String receivedData;
+    public String receviedHostAddress;
     public MulticastListener listener;
     
+    private DataReceivedEvent event;
+    
     /**
      *
      * @param ipAddress
-     * @param port
+     * @param udpPort
      * @throws java.io.IOException
      */
-    public Multicast(String ipAddress, int port) throws IOException {
-        buildSocket(ipAddress, port);
+    public Multicast(String ipAddress, int udpPort) throws IOException {
+        DATA_BUFFER = new byte[1024];
+        RECEIVED_PACKET = new DatagramPacket(DATA_BUFFER, DATA_BUFFER.length);
+        MULTICAST_GROUP_ADDRESS = InetAddress.getByName(ipAddress);
+        multicastSocket = new MulticastSocket(udpPort);
+        
+        buildMulticastSocket();
+    }
+    
+    public void buildMulticastSocket() throws IOException {
+        setMulticastSocketSettings();
+        joinMulticastGroup();
+    }
+    
+    private void setMulticastSocketSettings() throws IOException {
+        multicastSocket.setTimeToLive(TTL);
+    }
+    
+    private void joinMulticastGroup() throws IOException {
+        multicastSocket.joinGroup(MULTICAST_GROUP_ADDRESS);
     }
     
     /**
      *
-     * @param ipAddress
-     * @param port
      * @return
      * @throws IOException
      */
-    public MulticastSocket buildSocket(String ipAddress, int port) throws IOException {
-        InetAddress group = InetAddress.getByName(ipAddress);
-
-        mcastSocket = new MulticastSocket(port);
-        mcastSocket.setTimeToLive(16);
-        mcastSocket.joinGroup(group);
-        
-        System.out.println("Starting MulticastListener on: " + group.toString() + ":" + port);
-        return mcastSocket;
-    }
-    
-    /**
-     *
-     * @return
-     * @throws IOException
-     */
-    public synchronized Thread initReceiveData() throws IOException {
-        byte[] buffer = new byte[1024];
-                
-        DatagramPacket dataPacket = new DatagramPacket(buffer, buffer.length);
-        
+    public synchronized Thread receiveAndHandleData() throws IOException {
         while(true) {    
-            mcastSocket.receive(dataPacket);
-
-            dataPacketString = new String(
-                    dataPacket.getData(), dataPacket.getOffset(), dataPacket.getLength());                  
-            sourceIP = dataPacket.getAddress();
+            multicastSocket.receive(RECEIVED_PACKET);
+            processReceivedData();
             dataReceivedEvent();
-            //Thread.sleep(5000);
-        }        
+        }
+    }
+    
+    private void processReceivedData() {
+        receivedData = new String(
+                        RECEIVED_PACKET.getData(), 
+                        RECEIVED_PACKET.getOffset(), 
+                        RECEIVED_PACKET.getLength()
+                    );                  
+        receviedHostAddress = RECEIVED_PACKET.getAddress().getHostAddress();
+    }
+     
+    private synchronized void dataReceivedEvent() {
+        event = new DataReceivedEvent(this, receivedData, receviedHostAddress);
+        listener.dataReceived(event);
     }
     
     public void addDataReceivedListener(MulticastListener listener) {
         this.listener = listener;
-    }
-     
-    private synchronized void dataReceivedEvent() {
-        DataReceivedEvent event = new DataReceivedEvent(this, dataPacketString, sourceIP.getHostAddress());
-        listener.dataReceived(event);
     }
 }
