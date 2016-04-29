7e7f72582750bbb20f2e3e741a22d235eab60643
keithharper <keithharper1155@gmail.com>
2016-04-29 09:22:34 -0400

    Cleaned up a lot of code

--- NetworkUtils/src/com/network/utils/Multicast.java
+++ NetworkUtils/src/com/network/utils/Multicast.java
@@ -10,39 +10,36 @@ import java.net.MulticastSocket;
  * @author Keith Harper
  */
 public final class Multicast {    
-    private final byte[] DATA_BUFFER;
-    private final DatagramPacket RECEIVED_PACKET;
-    private final InetAddress MULTICAST_GROUP_ADDRESS;
+    private final byte[] DATA_BUFFER = new byte[1024];
+    private final DatagramPacket RECEIVED_PACKET = new DatagramPacket(DATA_BUFFER, DATA_BUFFER.length);
     private final int TTL = 16;
+    private final InetAddress MULTICAST_GROUP_ADDRESS;
+    private final int udpPort;
     
     public MulticastSocket multicastSocket;
     public String receivedData;
     public String receviedHostAddress;
     public MulticastListener listener;
     
-    private DataReceivedEvent event;
-    
     /**
      *
-     * @param ipAddress
-     * @param udpPort
+     * @param multicastGroupAddress IP Address of multicast group
+     * @param udpPort UDP port number to bind to
      * @throws java.io.IOException
      */
-    public Multicast(String ipAddress, int udpPort) throws IOException {
-        DATA_BUFFER = new byte[1024];
-        RECEIVED_PACKET = new DatagramPacket(DATA_BUFFER, DATA_BUFFER.length);
-        MULTICAST_GROUP_ADDRESS = InetAddress.getByName(ipAddress);
-        multicastSocket = new MulticastSocket(udpPort);
-        
+    public Multicast(String multicastGroupAddress, int udpPort) throws IOException {
+        this.MULTICAST_GROUP_ADDRESS = InetAddress.getByName(multicastGroupAddress);
+        this.udpPort = udpPort;
         buildMulticastSocket();
     }
     
-    public void buildMulticastSocket() throws IOException {
-        setMulticastSocketSettings();
+    private void buildMulticastSocket() throws IOException {
+        this.multicastSocket = new MulticastSocket(udpPort);
+        setMulticastSocketTTL();
         joinMulticastGroup();
     }
     
-    private void setMulticastSocketSettings() throws IOException {
+    private void setMulticastSocketTTL() throws IOException {
         multicastSocket.setTimeToLive(TTL);
     }
     
@@ -55,11 +52,12 @@ public final class Multicast {
      * @return
      * @throws IOException
      */
-    public synchronized Thread receiveAndHandleData() throws IOException {
+    public synchronized Thread receiveAndProcessPacket() throws IOException {
         while(true) {    
             multicastSocket.receive(RECEIVED_PACKET);
             processReceivedData();
-            dataReceivedEvent();
+            processReceivedHostAddress();
+            notifyDataReceivedListeners();
         }
     }
     
@@ -69,12 +67,14 @@ public final class Multicast {
                         RECEIVED_PACKET.getOffset(), 
                         RECEIVED_PACKET.getLength()
                     );                  
+    }
+    
+    private void processReceivedHostAddress() {
         receviedHostAddress = RECEIVED_PACKET.getAddress().getHostAddress();
     }
-     
-    private synchronized void dataReceivedEvent() {
-        event = new DataReceivedEvent(this, receivedData, receviedHostAddress);
-        listener.dataReceived(event);
+    
+    private synchronized void notifyDataReceivedListeners() {
+        listener.dataReceived(new DataReceivedEvent(this, receivedData, receviedHostAddress));
     }
     
     public void addDataReceivedListener(MulticastListener listener) {
